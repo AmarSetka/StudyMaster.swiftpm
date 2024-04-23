40d148bdfb0e3e68262b389153500b889cb39938
Amar Setka <asetka7368@stu.d214.org>
2024-04-23 12:42:19 -0500

    Made views

---
+++ FlashcardView.swift
@@ -0,0 +1,16 @@
+//
+//  FlashcardView.swift
+//  StudyMaster
+//
+//  Created by Amar Setka on 4/23/24.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct FlashcardView: View {
+    var body: some View {
+        Text("HI")
+    }
+}
---
+++ NoteTakingView.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakingView.swift
+//  StudyMaster
+//
+//  Created by Amar Setka on 4/23/24.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct NoteTakingView: View {
+    var body: some View {
+       Text("HI")
+    }
+}
+
